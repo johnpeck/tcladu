933a5d8a2186a5c3beb523229a3d390f93907bd5
John Peck <johnpeck@pop-os.localdomain>
2024-01-04 06:37:10 -0600

    Now I seem to be able to return a string, but is it correct?

--- src/adutcl.c
+++ src/adutcl.c
@@ -9,10 +9,7 @@
 #include <libusb-1.0/libusb.h>
 
 // ADU100 is a low-speed device, so we must use 8 byte transfers
-#define TRANSFER_SIZE    8     
-
-
-
+#define TRANSFER_SIZE    8
 
 int initialize() { return libusb_init(NULL); }
 
@@ -20,7 +17,7 @@ libusb_device_handle * open_device(int vid, int pid) {
   int result;
 
   struct libusb_device_handle * device_handle = NULL; // Our ADU's USB device handle
-  
+
   // Set debugging output to max level
   libusb_set_option( NULL, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_WARNING );
 
@@ -31,7 +28,7 @@ libusb_device_handle * open_device(int vid, int pid) {
     libusb_exit( NULL );
     exit( -2 );
   }
-  
+
   // Enable auto-detaching of the kernel driver.
   // If a kernel driver currently has an interface claimed, it will be automatically be detached
   // when we claim that interface. When the interface is restored, the kernel driver is allowed
@@ -51,13 +48,56 @@ libusb_device_handle * open_device(int vid, int pid) {
   return device_handle;
 }
 
+// This buffer will contain the return for reads. Its size is set to
+// the transfer size for low or full speed USB devices (ADU model
+// specific - see defines at top of file)
+// unsigned char buffer[ TRANSFER_SIZE ];
+
+// Read a command from an ADU device with a specified timeout
+int read_from_adu( libusb_device_handle * _device_handle, char * _read_str, int _read_str_len, int _timeout ) {
+  if ( _read_str == NULL || _read_str_len < 8 ) {
+    return -2;
+  }
+  
+
+  int bytes_read = 0;
+
+  // Buffer to hold the command we will receive from the ADU device
+  // Its size is set to the transfer size for low or full speed USB devices (ADU model specific - see defines at top of file)
+  unsigned char buffer[ TRANSFER_SIZE ];
+
+  // Zero out buffer to pad with null values (command buffer needs
+  // to be padded with 0s)
+  memset( buffer, 0, TRANSFER_SIZE );
+
+  // Attempt to read the result from the IN endpoint (0x81) with user specified timeout
+  int result = libusb_interrupt_transfer( _device_handle, 0x81, buffer, TRANSFER_SIZE, &bytes_read, _timeout );
+  printf( "Read result: %i, Bytes read: %u\n", result, bytes_read );
 
 
+  if ( result < 0 ) {
+    printf( "Error reading interrupt transfer: %s\n", libusb_error_name( result ) );
+    return result;
+  }
+
+  
+
+  // The buffer should now hold the data read from the ADU
+  // device. The first byte will contain 0x01, the remaining bytes
+  // are the returned value in string format. Let's copy the string
+  // from the read buffer, starting at index 1, to our _read_str
+  // buffer
+  memcpy( _read_str, &buffer[1], 7 );
+  buffer[7] = '\0'; // null terminate the string
+  printf( "Read value as string: %s\n", _read_str );
+
+  return result; // returns 0 on success, a negative number specifying the libusb error otherwise
+}
 
 // Write a command to an ADU device with a specified timeout
 int write_to_adu( libusb_device_handle * device_handle, const char * _cmd, int _timeout ) {
   // Get the length of the command string we are sending
-  const int command_len = strlen( _cmd ); 
+  const int command_len = strlen( _cmd );
 
   int bytes_sent = 0;
 
@@ -65,7 +105,7 @@ int write_to_adu( libusb_device_handle * device_handle, const char * _cmd, int _
   //
   // Its size is set to the transfer size for low or full speed USB
   // devices (ADU model specific - see defines at top of file)
-  unsigned char buffer[ TRANSFER_SIZE ]; 
+  unsigned char buffer[ TRANSFER_SIZE ];
 
   if ( command_len > TRANSFER_SIZE ) {
     printf( "Error: command is larger than our limit of %i\n", TRANSFER_SIZE );
@@ -74,15 +114,15 @@ int write_to_adu( libusb_device_handle * device_handle, const char * _cmd, int _
 
   // Zero out buffer to pad with null values (command buffer needs to
   // be padded with 0s)
-  memset( buffer, 0, TRANSFER_SIZE ); 
+  memset( buffer, 0, TRANSFER_SIZE );
 
   // First byte of the command buffer needs to be set to a decimal
   // value of 1
-  buffer[0] = 0x01; 
+  buffer[0] = 0x01;
 
   // Copy the command ASCII bytes into our buffer, starting at the
   // second byte (we need to leave the first byte as decimal value 1)
-  memcpy( &buffer[1], _cmd, command_len ); 
+  memcpy( &buffer[1], _cmd, command_len );
 
   // Attempt to send the command to the OUT endpoint (0x01) with the
   // use specified millisecond timeout
--- src/adutcl.i
+++ src/adutcl.i
@@ -11,7 +11,10 @@
   int initialize();
   libusb_device_handle *  open_device(int vid, int pid);
   int write_to_adu( libusb_device_handle * device_handle, const char * _cmd, int _timeout );
+  int read_from_adu( libusb_device_handle * _device_handle, char * _read_str, int _read_str_len, int _timeout );
 %}
+
+%cstring_bounded_output(char * _read_str, 1024);
 
 // Perform initialization when the module is loaded
 %init %{
@@ -24,5 +27,5 @@ int initialize();
 libusb_device_handle * open_device(int vid, int pid);
 
 int write_to_adu( libusb_device_handle * device_handle, const char * _cmd, int _timeout );
-
+int read_from_adu( libusb_device_handle * _device_handle, char * _read_str, int _read_str_len, int _timeout );
 
